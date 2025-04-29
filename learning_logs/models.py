e5b6c63dcb76db568a1f3309c8ea795d43daa07e
Marcelo Henrique Favaro <130109482+MarceloTIFavaro@users.noreply.github.com>
2025-04-29 16:35:26 -0300

    html em produção

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -21,6 +21,6 @@ class Entry (models.Model):
         verbose_name_plural = 'entries'
 
 
-        def __str__ (self):
-            """Devolve uma representação em string do modelo""" 
-            return self.text[:50] + '...'
+    def __str__ (self):
+        """Devolve uma representação em string do modelo""" 
+        return self.text[:50] + '...'
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def index(request):
+    """Página principal do Learning_log"""
+    return render(request, 'learning_logs/index.html')
+
+
 # Create your views here.

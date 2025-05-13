094397aaad4be6848fea9b4508e99d9c317f3f0d
Marcelo Henrique Favaro <130109482+MarceloTIFavaro@users.noreply.github.com>
2025-05-12 21:38:25 -0300

    forms

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 def index(request):
     """Página principal do Learning_log"""
@@ -19,3 +23,18 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries }
     return render(request, 'learning_logs/topic.html', context)
 
+def new_topic(request):
+    """Adiciona um novo assunto."""
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco 
+        form = TopicForm()
+    else: 
+        # Dados de POST submetidos; processa os dados 
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics')) 
+        
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)  
